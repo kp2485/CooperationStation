a6d0c105976bfb1f3a6ca8d1bd2038d7fc4ecdbc
Kyle Peterson <kdpeters@gmail.com>
2023-09-12 03:01:12 -0400

    Initial Commit

---
+++ CooperationStation/CooperationStationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CooperationStationApp.swift
+//  CooperationStation
+//
+//  Created by Kyle Peterson on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CooperationStationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
